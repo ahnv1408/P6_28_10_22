60af251011062e1fedc3679a652dde9a6457de4c
Alicia <alicia2.haumann@gmail.com>
2023-01-02 20:21:14 +0100

    final commit

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const password = require ('../middleware/password.js');
 // IMPORTATION CONTROLLERS
 const userCtrl = require('../controllers/user');
 
 // ROUTE
-router.post('/signup', userCtrl.signup);
+router.post('/signup', password, userCtrl.signup);
 router.post('/login', userCtrl.login);
 
 module.exports = router;
